347f6ff9662aca766fcd365d332860301472e94d
Joshua John <successfuljosh1@gmail.com>
2019-08-10 05:00:32 +0100

    latest

--- Reflections/Reflections.Android/obj/Debug/81/android/src/mono/android/app/XamarinAndroidEnvironmentVariables.java
+++ Reflections/Reflections.Android/obj/Debug/81/android/src/mono/android/app/XamarinAndroidEnvironmentVariables.java
@@ -5,7 +5,7 @@ public class XamarinAndroidEnvironmentVariables
 	// Variables are specified the in "name", "value" pairs
 	public static final String[] Variables = new String[] {
 		"MONO_LOG_LEVEL", "info",
-		"XAMARIN_BUILD_ID", "d7608679-4da4-4926-8524-ba10cc93cdf8",
+		"XAMARIN_BUILD_ID", "c8c1e2ec-27c2-4985-b191-45a369dfbcbf",
 		"XA_HTTP_CLIENT_HANDLER_TYPE", "Xamarin.Android.Net.AndroidClientHandler",
 		"XA_TLS_PROVIDER", "btls",
 		"MONO_GC_PARAMS", "major=marksweep-conc",
